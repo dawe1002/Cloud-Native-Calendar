5ed71ad96f3eadfc083f72a102068d3bcf649e99
dawe1002 <dawe1002@stud.hs-kl.de>
2025-01-25 07:42:50 +0100

    Beschreibung deiner Änderungen

--- frontend/public/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('/api/calendar')
-    .then((response) => response.json())
-    .then((data) => {
-      const calendarDiv = document.getElementById('calendar');
-      calendarDiv.innerText = data.message;
-    })
-    .catch((error) => console.error('Fehler beim Laden der Kalenderdaten:', error));
-});
